6bd56d11f7a6d81556b08250561e850406e2d9b9
Daniel Homer <daniel.homer6@gmail.com>
2016-06-10 20:05:07 -0400

    add columns to create_tags migration

--- db/migrate/20160610235822_create_tags.rb
+++ db/migrate/20160610235822_create_tags.rb
@@ -1,6 +1,8 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
+      t.string :name
+      t.references :taggable, polymorphic: true, index: true
 
       t.timestamps null: false
     end
